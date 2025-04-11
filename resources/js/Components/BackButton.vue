af52be491f4e85e5ef6b7a62b3eb0ae8f6b5a6ad
Joe Downey <joedowney@gmail.com>
2025-04-11 14:33:54 -0400

    hovers

--- resources/js/Components/BackButton.vue
+++ resources/js/Components/BackButton.vue
@@ -15,7 +15,7 @@ let back = () => {
 </script>
 
 <template>
-    <a v-if="stack.length > 1" href="#" @click.prevent="back" class="p-3 flex items-center bg-gray-800 rounded-full h-10 w-10">
+    <a v-if="stack.length > 1" href="#" @click.prevent="back" class="p-3 flex items-center bg-gray-800 rounded-full h-10 w-10  hover:bg-gray-700 transition duration-200">
         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" style="fill:#ffffff"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
     </a>
     <span v-else class="p-3 flex items-center bg-gray-800 rounded-full h-10 w-10">
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,17 +13,17 @@ import Page from "@/Components/Page.vue";
         </div>
 
         <div class="md:grid grid-cols-3 gap-4 mb-8">
-            <Link href="/bands" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6">
+            <Link href="/bands" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6 transition duration-200">
                 <h3>Bands</h3>
                 <p>Browse bands by name</p>
             </Link>
 
-            <Link href="/cities" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6">
+            <Link href="/cities" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6 transition duration-200">
                 <h3>City</h3>
                 <p>Browse bands by city</p>
             </Link>
 
-            <Link href="/genres" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6">
+            <Link href="/genres" class="rounded-lg bg-gray-800 p-4 hover:bg-gray-700 block mb-6 transition duration-200">
                 <h3>Genre</h3>
                 <p>Browse bands by genre</p>
             </Link>
